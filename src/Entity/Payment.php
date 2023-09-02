fbbff3f5a23687b957fee8d399665d0b5b0c6feb
matiw219 <mateusz.wielechowski1@gmail.com>
2023-09-02 16:31:55 +0200

    Add Payment entity

---
+++ src/Entity/Payment.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PaymentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PaymentRepository::class)]
+class Payment
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $appName = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column(length: 30)]
+    private ?string $status = null;
+
+    #[ORM\Column]
+    private ?int $userId = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAppName(): ?string
+    {
+        return $this->appName;
+    }
+
+    public function setAppName(string $appName): static
+    {
+        $this->appName = $appName;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getUserId(): ?int
+    {
+        return $this->userId;
+    }
+
+    public function setUserId(int $userId): static
+    {
+        $this->userId = $userId;
+
+        return $this;
+    }
+}
